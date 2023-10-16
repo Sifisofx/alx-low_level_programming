92529995c3acb6582ff2d3130b3da211c34652be
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-16 16:24:45 -0700

    kj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - this is the main function
  * return: 1 always
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof( "and that piece of art is useful\" - Dora Korpar, 2015-10-19"));

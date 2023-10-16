7e74b80af4e02c3e1b2bd29051d13d70c66f8274
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-16 16:33:27 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - this is the main function
- * return: 1 always
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof( "and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+        return (1);
 }
+

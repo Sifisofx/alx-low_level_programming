10fffcd83d3cefe9a741bac34e0557078235c832
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-17 12:51:05 -0700

    k

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

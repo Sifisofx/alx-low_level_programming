4edf4a1f44a5513f6a15e303b60637a4dac127d4
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-18 03:15:42 -0700

    f

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

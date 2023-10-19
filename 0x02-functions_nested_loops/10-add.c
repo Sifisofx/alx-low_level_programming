d3b5e3bfe4164baa42dc295ffdbf45ee13396fb7
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-19 07:12:40 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds 2 value
+ * @p: The number to be checked first
+ * @l: the second number to be checked
+ * Return: an integer
+ */
+
+int add(int p, int l)
+{
+	return (p + l);
+}

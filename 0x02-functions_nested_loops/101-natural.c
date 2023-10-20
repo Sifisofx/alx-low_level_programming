e86a33e1129d788cc27f68b971fd3bd141c4450a
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-20 10:41:41 -0700

    d

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#iclude <stdio>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i, o = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			o += i;
+		}
+		i++;
+	}
+	printf("%d\n", o);
+	return (0);
+}

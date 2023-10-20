803d4f4ce36069ee197d0efcfdee51d2e293b9f0
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-20 13:02:28 -0700

    s

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+/**
+ * main - tests function that prints if an int is positive or negative
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_negative(i);
+	return (0);
+}

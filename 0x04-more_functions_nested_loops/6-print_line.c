5b2a0050416a83119871a5ddd553fa74f24b50f8
Sifiso_Mbiza <sifisombiza2@gmail.com>
2023-10-23 04:33:09 -0700

    d

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: to be checked
+ * Return: the value n
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+			_putchar(95);
+	}
+	_putchar('\n');
+}
